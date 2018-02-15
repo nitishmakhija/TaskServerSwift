de700dd5c71be8be3594f20dbb5c80ed168a9caa
Marcin Czachurski <mczachurski@icloud.com>
2018-02-15 22:12:23 +0100

    Add file fot Tasks controller tests.

---
+++ Tests/TaskerServerLibTests/Controllers/TasksControllerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TasksControllerTests.swift
+//  TaskerServerLibTests
+//
+//  Created by Marcin Czachurski on 15.02.2018.
+//
+
+import Foundation
+
+import XCTest
+import PerfectHTTP
+import Dobby
+@testable import TaskerServerLib
+
+class TasksControllerTests: XCTestCase {
+
+}
