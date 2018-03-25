f19452cc3354d1930051e80853bf5f6438717e65
Marcin Czachurski <mczachurski@icloud.com>
2018-03-25 20:21:47 +0200

    Remove not required packages.

--- Package.swift
+++ Package.swift
@@ -13,8 +13,7 @@ let package = Package(
         .package(url: "https://github.com/IBM-Swift/Configuration.git", from: "3.0.1"),
         .package(url: "https://github.com/kjessup/Perfect-SQLite.git", .branch("master")),
         .package(url: "https://github.com/PerfectlySoft/Perfect-Crypto.git", from: "3.0.0"),
-        .package(url: "https://github.com/IBM-Swift/FileKit.git", from: "0.0.1"),
-        .package(url: "https://github.com/Digipolitan/perfect-middleware-swift.git", from: "1.0.0")
+        .package(url: "https://github.com/IBM-Swift/FileKit.git", from: "0.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
